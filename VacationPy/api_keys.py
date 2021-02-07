1a469bd35b1b8b70de0ad5eb46c2e445acb6c265
graceloveora123 <grace.bo.sun06@gmail.com>
2021-02-07 09:41:18 -0500

    On branch main

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "acffccfa25853506baf4ac3c7880439c"
+
+# Google API Key
+g_key = "AIzaSyCF1rzpgvjxt9D_3h8Yd0jUxNStokX9PgA"
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "acffccfa25853506baf4ac3c7880439c"
+
+# Google API Key
+g_key = "AIzaSyDCA6KyuPBpSFGU-TlonDGLu94gevuGINU"
