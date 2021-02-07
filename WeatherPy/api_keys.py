08c4125327d5ad936a30183e8bbdb7f4eb848dcf
graceloveora123 <65875487+graceloveora123@users.noreply.github.com>
2021-02-07 09:44:36 -0500

    Update api_keys.py

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "acffccfa25853506baf4ac3c7880439c"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyDCA6KyuPBpSFGU-TlonDGLu94gevuGINU"
+g_key = ""
