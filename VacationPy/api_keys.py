1e5168e470413dd79a36f8318c2330e5938a9f25
graceloveora123 <65875487+graceloveora123@users.noreply.github.com>
2021-02-07 09:44:00 -0500

    Update api_keys.py

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "acffccfa25853506baf4ac3c7880439c"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyCF1rzpgvjxt9D_3h8Yd0jUxNStokX9PgA"
+g_key = ""
